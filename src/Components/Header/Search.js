abfb25861ba29dc8a459dd3a8f6e0d2799cecbbc
DyaryRaoof <dyary2020@gmail.com>
2022-02-01 21:37:59 +0300

    add search input

---
+++ src/Components/Header/Search.js
@@ -0,0 +1,7 @@
+const Search = () => (
+  <div className="mx-auto w-75 ">
+    <input type="email" className="form-control w-100" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search" />
+  </div>
+);
+
+export default Search;
