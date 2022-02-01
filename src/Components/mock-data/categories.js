8ba12ea3d319de53873f5c1114fa948fe642d29d
DyaryRaoof <dyary2020@gmail.com>
2022-02-01 21:38:53 +0300

    Add categories and items mock data

---
+++ src/Components/mock-data/categories.js
@@ -0,0 +1,232 @@
+const categories = [
+  {
+    id: 0,
+    name: 'Category 0',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+  {
+    id: 1,
+    name: 'Category 1',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 2,
+    name: 'Category 2',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 3,
+    name: 'Category 3',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 4,
+    name: 'Category 4',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 5,
+    name: 'Category 5',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+  {
+    id: 6,
+    name: 'Category 6',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 7,
+    name: 'Category 7',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+  {
+    id: 8,
+    name: 'Category 8',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 9,
+    name: 'Category 9',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+  {
+    id: 10,
+    name: 'Category 10',
+    subcategories:
+            [
+              {
+                id: 0,
+                name: 'SubCategory 0',
+              },
+              {
+                id: 1,
+                name: 'SubCategory 1',
+              },
+              {
+                id: 2,
+                name: 'SubCategory 2',
+              },
+            ],
+
+  },
+
+];
+
+export default categories;
