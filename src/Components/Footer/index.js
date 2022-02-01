c362e0951f21bbdef15ae46f1fff283087485d77
DyaryRaoof <dyary2020@gmail.com>
2022-02-01 21:36:57 +0300

    add footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,37 @@
+const Footer = () => (
+  <footer className="orange">
+    <hr />
+    <div className="container">
+      <div className="d-flex justify-content-between">
+        <p><a className="orange" href="/#">English</a></p>
+        <p><a className="orange" href="/#">Adds</a></p>
+      </div>
+      <div className="d-flex justify-content-between orange">
+        <p className="orange">
+          © 2022 Copyright:
+          <a className="orange" href="/#">Kurd Mall</a>
+        </p>
+        <p>
+          <a className="orange" href="/#">Privacy Policy</a>
+        </p>
+        <p>
+          <a className="orange" href="/#">Terms & Conditions</a>
+        </p>
+        <p>
+          <a className="orange" href="/#">Contact Us</a>
+        </p>
+        <p>
+          <a className="orange" href="/#">About Us</a>
+        </p>
+        <p>
+          <a className="orange" href="/#">FAQ</a>
+        </p>
+        <p>
+          <a className="orange" href="/#">Help</a>
+        </p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
