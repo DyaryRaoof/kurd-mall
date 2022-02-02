21cc07092fbec6e9a13bcfe46974b4e95b416110
DyaryRaoof <dyary2020@gmail.com>
2022-02-02 03:07:39 +0300

    Fix footer styling in modbile

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,34 +1,47 @@
 const Footer = () => (
   <footer className="orange">
     <hr />
-    <div className="container">
-      <div className="d-flex justify-content-between">
-        <p><a className="orange" href="/#">English</a></p>
-        <p><a className="orange" href="/#">Adds</a></p>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col-md-6" />
+        <div className="col-md-6" />
       </div>
-      <div className="d-flex justify-content-between orange">
-        <p className="orange">
-          © 2022 Copyright:
-          <a className="orange" href="/#">Kurd Mall</a>
-        </p>
-        <p>
-          <a className="orange" href="/#">Privacy Policy</a>
-        </p>
-        <p>
-          <a className="orange" href="/#">Terms & Conditions</a>
-        </p>
-        <p>
-          <a className="orange" href="/#">Contact Us</a>
-        </p>
-        <p>
-          <a className="orange" href="/#">About Us</a>
-        </p>
-        <p>
-          <a className="orange" href="/#">FAQ</a>
-        </p>
-        <p>
-          <a className="orange" href="/#">Help</a>
-        </p>
+      <div className="orange text-center">
+        <div className="row">
+          <div className="col-md-4">
+            <p className="orange">
+              © 2022 Copyright:
+              <a className="orange" href="/#">Kurd Mall</a>
+            </p>
+            <p>
+              <a className="orange" href="/#">Terms & Conditions</a>
+            </p>
+            <p>
+              <a className="orange" href="/#">Privacy Policy</a>
+            </p>
+
+          </div>
+          <div className="col-md-4">
+            <p><a className="orange" href="/#">English</a></p>
+
+            <p>
+              <a className="orange" href="/#">Contact Us</a>
+            </p>
+            <p>
+              <a className="orange" href="/#">About Us</a>
+            </p>
+          </div>
+          <div className="col-md-4">
+            <p><a className="orange" href="/#">Adds</a></p>
+            <p>
+              <a className="orange" href="/#">FAQ</a>
+            </p>
+            <p>
+              <a className="orange" href="/#">Help</a>
+            </p>
+          </div>
+        </div>
+
       </div>
     </div>
   </footer>
