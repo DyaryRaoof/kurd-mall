9bdce46f8d6a5c0e08eeaf64c661aa4054858c74
DyaryRaoof <dyary2020@gmail.com>
2022-02-10 18:05:21 +0300

    Add select item variant to item detail page

--- src/Components/ItemDetail/index.js
+++ src/Components/ItemDetail/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import MaterialIcon from '../Shared/MateriaIcon';
 import Carousel from '../StoreDetailPage/Carousel';
@@ -5,72 +6,76 @@ import './ItemDetail.css';
 import Stars from '../Shared/Stars';
 import RoundOrangeIconButton from '../Shared/RoundOrangeIconButton';
 
-const ItemDetail = ({ item }) => (
-  <div>
-    <div className="container">
-      <div className="row">
+const ItemDetail = ({ item }) => {
+  const [selectedVariant, setSelectedVariant] = useState(item.variants[0]);
 
-        <div className="d-flex justify-content-between position relative item-detail-top-icons">
-          <MaterialIcon text="share" orange isLarge />
-          <MaterialIcon text="store" orange isLarge />
-        </div>
-        <Carousel images={[
-          item.image, item.image, item.image,
-          item.image, item.image, item.image,
-        ]}
-        />
-        <div className="row mx-2">
-          <div className="col-md-6 mt-3">
-            <div className="row  gray-background rounded mx-2">
-              <h3>{item.name}</h3>
-              <div className="col-6">
-                <div>
-                  <div className="orange">
-                    {`${item.price.value} ${item.price.currency} 
-                + ${item.shippingPrice.value} ${item.shippingPrice.currency} shipping`}
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+
+          <div className="d-flex justify-content-between position relative item-detail-top-icons">
+            <MaterialIcon text="share" orange isLarge />
+            <MaterialIcon text="store" orange isLarge />
+          </div>
+          <Carousel images={[
+            item.image, item.image, item.image,
+            item.image, item.image, item.image,
+          ]}
+          />
+          <div className="row mx-2">
+            <div className="col-md-6 mt-3">
+              <div className="row  gray-background rounded mx-2">
+                <h3>{item.name}</h3>
+                <div className="col-6">
+                  <div>
+                    <div className="orange">
+                      {`${item.price.value} ${item.price.currency} 
+                  + ${item.shippingPrice.value} ${item.shippingPrice.currency} shipping`}
+                    </div>
+                    <div className="orange">{`${item.price.value + item.shippingPrice.value} ${item.price.currency} total`}</div>
                   </div>
-                  <div className="orange">{`${item.price.value + item.shippingPrice.value} ${item.price.currency} total`}</div>
+
                 </div>
-
+                <div className="col-6 ">
+                  <Stars number={item.stars.count} users={item.stars.users} />
+                  <div className="text-danger">{`Only ${item.leftInStock} are left in stock `}</div>
+                </div>
+                <div className="mt-2">{item.description}</div>
               </div>
-              <div className="col-6 ">
-                <Stars number={item.stars.count} users={item.stars.users} />
-                <div className="text-danger">{`Only ${item.leftInStock} are left in stock `}</div>
-              </div>
-              <div className="mt-2">{item.description}</div>
             </div>
-          </div>
 
-          <div className="col-md-6 mt-3">
-            <div className="gray-background item-detail-variants-wrapper p-3 mx-2">
-              {item.variants.map((variant) => (
-                <button key={variant.id} type="button" className="icon-button">
-                  <div className="item-detail-variants">
-                    <div className="item-detail-variant-image">
-                      <img src={item.images[variant.imageIndex]} alt={variant.name} className="variant-image" />
+            <div className="col-md-6 mt-3">
+              <div className="gray-background item-detail-variants-wrapper p-3 mx-2">
+                {item.variants.map((variant) => (
+                  <button key={variant.id} type="button" className={`icon-button ${selectedVariant === variant ? 'border border-danger border-2' : ''}`} onClick={() => setSelectedVariant(variant)}>
+                    <div className="item-detail-variants">
+                      <div className="item-detail-variant-image">
+                        <img src={item.images[variant.imageIndex]} alt={variant.name} className="variant-image" />
+                      </div>
+                      <div className="item-detail-variant-name">{variant.name}</div>
+                      <div className="item-detail-variant-price">
+                        {`${variant.price.value} ${variant.price.currency}`}
+                      </div>
                     </div>
-                    <div className="item-detail-variant-name">{variant.name}</div>
-                    <div className="item-detail-variant-price">
-                      {`${variant.price.value} ${variant.price.currency}`}
-                    </div>
-                  </div>
-                </button>
-              ))}
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="d-flex justify-content-center align-items-center flex-column mt-5">
-        <button className="icon-button" type="button">
-          <RoundOrangeIconButton buttonText="Add to cart" iconName="add_shopping_cart" />
-        </button>
-        <button type="button" className="icon-button mt-3">
-          <RoundOrangeIconButton buttonText="Message Seller" iconName="mode_comment" />
-        </button>
+        <div className="d-flex justify-content-center align-items-center flex-column mt-5">
+          <button className="icon-button" type="button">
+            <RoundOrangeIconButton buttonText="Add to cart" iconName="add_shopping_cart" />
+          </button>
+          <button type="button" className="icon-button mt-3">
+            <RoundOrangeIconButton buttonText="Message Seller" iconName="mode_comment" />
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 ItemDetail.propTypes = {
   item: PropTypes.shape({
