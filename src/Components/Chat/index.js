cfbd2d7bf884cf030bec6325dacf6dad68b777b9
DyaryRaoof <dyary2020@gmail.com>
2022-02-12 00:14:00 +0300

    Add chat input to chat page

--- src/Components/Chat/index.js
+++ src/Components/Chat/index.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import './Chat.css';
 import Bubble from './Bubble';
 import chats from '../mock-data/chats';
 
-const Chat = () => (
-  <main>
-    <div>
-      <h2 className="orange mb-4 ms-2">Chat</h2>
-      {chats.map((chat) => (
-        <Bubble key={chat.id} chat={chat} />
-      ))}
-    </div>
-  </main>
-);
+const Chat = () => {
+  const [message, setMessage] = useState('');
+
+  return (
+    <main>
+      <div>
+        <h2 className="orange mb-4 ms-2">Chat</h2>
+        <div className="mx-2">
+          <input className="rounded-pill p-3 mb-2  form-control" type="text" placeholder="Enter Message" value={message} onChange={(e) => setMessage(e.target.value)} />
+        </div>
+        {chats.map((chat) => (
+          <Bubble key={chat.id} chat={chat} />
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export default Chat;
